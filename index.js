989c2375dcd19fa19bbddaca5db0962d76d87611
Axel Obscura <axosar@gmail.com>
2020-05-05 08:30:38 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Main starting point
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+
+//App setup
+
+//Server setup
+const port = process.env.PORT || 3090;

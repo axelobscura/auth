4e7590646fd927b2c6e8d96173518848caaf657e
Axel Obscura <axosar@gmail.com>
2020-05-05 11:57:38 -0500

    Connection

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,30 @@
+const User = require('../models/user');
+
 exports.signup = function (req, res, next) {
-    res.send({ success: true })
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // see if a user does exist
+    User.findOne({ email: email }, function (err, existingUser) {
+        if (err) { return next(err); }
+        // if email exists return error
+        if (existingUser) {
+            return res.status(422).send({ error: "Email in use" });
+        };
+        // if email/user does not exists
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function (err) {
+            if (err) { return next(err) };
+
+            // Respond to request
+            res.json({ success: true });
+        })
+
+
+
+    });
 }
--- index.js
+++ index.js
@@ -8,15 +8,8 @@ const router = require('./router');
 const mongoose = require('mongoose');
 
 //DB Setup
-//mongoose.connect('mongodb+srv://axosar@admin:LjqhtCOj5b3BuiUa@cluster0-muwar.mongodb.net/test?retryWrites=true&w=majority')
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://axosar@admin:LjqhtCOj5b3BuiUa@cluster0-muwar.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("auth").collection("users");
-    //console.log(collection);
-    client.close();
+mongoose.connect('mongodb+srv://axosar:LjqhtCOj5b3BuiUa@cluster0-muwar.mongodb.net/auth?retryWrites=true&w=majority', {
+    useUnifiedTopology: true
 });
 
 //App setup

f4a8bdba9d5c86fecab660186e659215454655e3
Axel Obscura <axosar@gmail.com>
2020-05-05 08:34:24 -0500

    Initial setup for server

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ const app = express();
 
 //Server setup
 const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on: ", port)

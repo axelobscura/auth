7fd4e859848943df516af4e2af604401ec051ac0
Axel Obscura <axosar@gmail.com>
2020-05-07 14:23:14 -0500

    Implementing signin with localStrategy

--- models/user.js
+++ models/user.js
@@ -19,7 +19,15 @@ userSchema.pre('save', function (next) {
             next();
         })
     })
-})
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) { return callback(err); }
+
+        callback(null, isMatch);
+    })
+}
 
 // Create the model
 const ModelClass = mongoose.model('user', userSchema);
--- services/passport.js
+++ services/passport.js
@@ -3,11 +3,22 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     secretOrKey: config.secret
 };
+
+// Setupr passport local startegy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+    // Verify email and password
+    User.findOne({ email: email }, function (err, user) {
+        if (err) { return done(ewrr); }
+        if (!user) { return done(null, false); }
+    })
+})
 
 //Create jwt strategy
 const jwtLogin = new JwtStategy(jwtOptions, function (payload, done) {
